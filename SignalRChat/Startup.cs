166d8194f6b462cba5ed18911895eebbd1c9dfda
Miraç Kılıç <mirac.kilic@rlcteknoloji.com>
2021-03-14 01:56:28 +0300

    redis package added

--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -20,7 +20,7 @@ namespace SignalRChat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddSignalR();
+            services.AddSignalR().AddStackExchangeRedis("<your_Redis_connection_string>");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +48,7 @@ namespace SignalRChat
             {
                 endpoints.MapRazorPages();
                 endpoints.MapHub<ChatHub>("/chathub");
-            });
+            });            
         }
     }
 }
